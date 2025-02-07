6ab47031733a7aebfccb83e09b5d8662aa014718
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2025-02-07 21:50:59 +0900

    メインファイルを作成

---
+++ 07_VirtualMachine_StackArithmetic/main.py
@@ -0,0 +1,19 @@
+class Parser:
+    def __init__(self) -> None:
+        pass
+
+    def has_more_lines(self) -> bool:
+        pass
+
+    def advance(self) -> None:
+        pass
+
+    def commnad_type(self)
+
+class CodeWriter:
+    pass
+
+
+class VMTranslator:
+    pass
+
