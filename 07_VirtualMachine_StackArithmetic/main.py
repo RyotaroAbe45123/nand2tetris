42ff33c92df4e24043a0e989ce8a453f923830c0
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2025-02-08 00:29:03 +0900

    add class

--- 07_VirtualMachine_StackArithmetic/main.py
+++ 07_VirtualMachine_StackArithmetic/main.py
@@ -1,3 +1,18 @@
+from enum import Enum
+
+
+class Command(Enum):
+    ARITHMETIC = "C_ARITHMETIC"
+    PUSH = "C_PUSH"
+    POP = "C_POP"
+    LABEL = "C_LABEL"
+    GOTO = "C_GOTO"
+    IF = "C_IF"
+    FUNCTION = "C_FUNCTION"
+    RETURN = "C_RETURN"
+    CALL = "C_CALL"
+
+
 class Parser:
     def __init__(self) -> None:
         pass
@@ -8,10 +23,28 @@ class Parser:
     def advance(self) -> None:
         pass
 
-    def commnad_type(self)
+    def commnad_type(self) -> Command:
+        pass
+    
+    def arg1(self) -> str:
+        pass
+    
+    def arg2(self) -> int:
+        pass
+
 
 class CodeWriter:
-    pass
+    def __init__(self, file_path: str) -> None:
+        pass
+    
+    def write_arithmetic(self, command: str) -> None:
+        pass
+    
+    def write_push_pop(self, command: Command, segment: str, index: int) -> None:
+        pass
+    
+    def close(self) -> None:
+        pass
 
 
 class VMTranslator:
